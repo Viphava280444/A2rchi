60fbe873022ef38f79894fcdd2c547821ac5c389
Ohm Viphava <viphava.khl@outlook.com>
2026-02-05 14:10:14 -0600

    Update cli_main.py

--- src/cli/cli_main.py
+++ src/cli/cli_main.py
@@ -28,7 +28,7 @@ from src.utils.logging import get_logger, setup_cli_logging
 from src.cli.tools.config_seed import seed_entry
 import subprocess
 
-# DEFINITIONS
+# DEFINITIONS5555
 env = Environment(
     loader=PackageLoader("src.cli"),
     autoescape=select_autoescape(),
