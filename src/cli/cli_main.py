1be1b9bfee5003d930724160434cdc942bfe0b74
Ohm Viphava <viphava.khl@outlook.com>
2026-02-05 14:28:29 -0600

    Update cli_main.py

--- src/cli/cli_main.py
+++ src/cli/cli_main.py
@@ -18,7 +18,7 @@ from src.cli.utils.helpers import *
 from src.cli.utils.service_builder import ServiceBuilder
 from src.utils.logging import get_logger, setup_cli_logging
 
-# DEFINITIONS 666
+# DEFINITIONS 666 7777
 env = Environment(
     loader=PackageLoader("src.cli"),
     autoescape=select_autoescape(),
